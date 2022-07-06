3c8caa53c65b1f5093c299f542d20d9dcd336e7c
shkippppper <a.titberidze@yahoo.com>
2022-07-06 13:39:15 +0400

    Done

---
+++ 5kyu/LuckCheck/LuckCheck.py
@@ -0,0 +1,13 @@
+def luck_check(string):
+    if string.isnumeric():    
+            length = len(string)//2
+            leftside = 0
+            rightside = 0
+            for i in range(len(string)//2):
+                leftside += int(string[i])  
+            for j in range(len(string)//2):
+                rightside += int(string[-length+j])
+            return rightside == leftside
+    else:
+        raise TypeError("nope nope")
+    
