b6e9df0d02ddebfc412769d4cc37f0d4fe29a0af
shkippppper <a.titberidze@yahoo.com>
2022-07-07 16:07:47 +0400

    Done

--- 4kyu/HowManyNumbersIII/HowManyNumbersIII.py
+++ 4kyu/HowManyNumbersIII/HowManyNumbersIII.py
@@ -1,30 +1,35 @@
+# # # # # # # # # # # # # # # # # # # # # # # # 
+# # THIS ONE IS PRETTY SLOW
+# # # # # # # # # # # # # # # # # # # # # # # # 
+
+
 def find_all(sum_dig, digs):
     myList = []
     isMore = False
-    for i in range(pow(10,digs)-pow(10,digs-1)):              #loop through all the numbers with the given digits
+    for i in range(pow(10,digs)-pow(10,digs-1)):               #loop through all the numbers with the given digits
         maxDigit = 0            
         sumOfDigits = 0                                         #restart the sumofdigits
-        number = i+pow(10,digs-1)                      #loop through the numbers
-        for j in range(digs):                                      #get the sum of digits
+        number = i+pow(10,digs-1)                               #loop through the numbers
+        if "0" in str(number):
+            continue
+        for l in range(digs):
+            if int(str(number)[l]) >= maxDigit:
+                isMore = True
+            else: 
+                isMore = False 
+                break 
+            maxDigit = int(str(number)[l])
+        for j in range(digs):                         #get the sum of digits
             sumOfDigits += int(str(number)[j])          
         if sumOfDigits == sum_dig:                              #check if sum of digits is equal to given sum
-            for l in range(digs):                             #check if the given number is in ascending order
-                if int(str(number)[l]) >= maxDigit:
-                    isMore = True
-                else: 
-                    isMore = False 
-                    break
-                maxDigit = int(str(number)[l])
-
             if isMore:
                 myList.append(number)
     if len(myList) == 0:
         return []
         
-    return [len(myList),min(myList),max(myList)]
+    return [len(myList),min(myList),max(myList)]#, myList
 
 
 
-print(find_all(20,2))
-    
+print(find_all(22,7))
 
