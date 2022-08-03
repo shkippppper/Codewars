e244afec6c52c9a92752bb58137deb5445c3f85f
shkippppper <a.titberidze@yahoo.com>
2022-08-03 23:36:57 +0400

    Done

--- 5kyu/MovingZerosToTheEnd/MovingZerosToTheEnd.py
+++ 5kyu/MovingZerosToTheEnd/MovingZerosToTheEnd.py
@@ -8,4 +8,7 @@ def move_zeros(array):
     for i in range(rangeToGo):
         array.append(0)
     return (array)
-move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9])
+print(move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
+
+
+
